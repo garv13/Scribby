3d3f6cdd435cbca36b55696eb9ee5b5d9d5ba18f
garvj <garvjasuja@outlook.com>
2017-03-18 16:45:45 +0530

    Classes and pages added

---
+++ Scribby/Note.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scribby
+{
+    class Note
+    {
+        public string UserId { get; set; }
+
+        public bool store { get; set; }
+
+        public string PackId { get; set; }
+
+        public string gpr_coordinate { get; set; }
+
+        public string Media_Type { get; set; }
+
+        public int Like { get; set; }
+
+        public double Yaw { get; set; }
+
+        public double Pitch { get; set; }
+
+        public string Media_Url { get; set; }
+
+        public string User_Access { get; set; }
+    }
+}
---
+++ Scribby/Pack.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scribby
+{
+    class Pack
+    {
+        public string PackId { get; set; }
+
+        public string UserId { get; set; }
+
+        public int No_Of_Purchases { get; set; }
+
+        public float Price { get; set; }
+
+        public string Icon_Url { get; set; }
+
+    }
+}
