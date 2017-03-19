3c94bf093c60e670eb42e0a29236f20d161ffad3
garvj <garvjasuja@outlook.com>
2017-03-19 16:40:05 +0530

    store page uploadd

--- Scribby/Pack.cs
+++ Scribby/Pack.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml.Media.Imaging;
 
 namespace Scribby
 {
     class Pack
     {
+        public BitmapImage Image { get; set; }
+        public string Title { get; set; }
         public bool InStore { get; set; }
         public string Id { get; set; }
 
